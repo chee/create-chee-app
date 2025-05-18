dcc671ced04128aec2c9b882c2603f286eafe603
chee <yay@chee.party>
2025-05-18 21:13:57 +0100

    make children optional in Chrome

--- template/src/ui/layouts/chrome.tsx
+++ template/src/ui/layouts/chrome.tsx
@@ -2,7 +2,7 @@ import type {JSX} from "solid-js"
 import {ToastRegion} from "../components/toast.tsx"
 import ServiceWorker from "../components/service-worker.tsx"
 
-export default function Chrome(props: {children: JSX.Element}) {
+export default function Chrome(props: {children?: JSX.Element}) {
 	return (
 		<div class="chrome">
 			{props.children}
